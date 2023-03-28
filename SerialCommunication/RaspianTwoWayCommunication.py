cf74b3b346212a91f71b1d0fd5304e520d509c47
Ojiako <kso170000@utdallas.edu>
2023-03-28 15:33:09 -0500

    Serial communication stuff

--- SerialCommunication/RaspianTwoWayCommunication.py
+++ SerialCommunication/RaspianTwoWayCommunication.py
@@ -1,5 +1,9 @@
-def instantiate_serial_communication():
-    pass
+import serial
+
+
+def instantiate_serial_communication(arduino_path: str, speed: int, timeout: int):
+    serializer = RaspbianSerializer(arduino_path, speed, timeout)
+    return serializer
 
 
 def send_info_to_arduino():
@@ -8,3 +12,15 @@ def send_info_to_arduino():
 
 def receive_info_from_arduino():
     pass
+
+
+class RaspbianSerializer:
+    def __init__(self, arduino_path: str, speed: int, timeout: int):
+        self.ser = serial.Serial(arduino_path, speed, timeout)
+        self.ser.reset_input_buffer()
+        pass
+
+
+
+
+    
